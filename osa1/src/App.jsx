ef6f3829e9c8d1c17024330acc1635caf847c695
EetuKV <eetu5294@gmail.com>
2025-04-03 16:00:37 +0300

    Osa 1b

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -13,7 +13,7 @@ const Header = (props) => (<><h1>{props.course}</h1>
  * @returns listauksen osien nimistä ja tehtävien lukumääristä kappale-elementteinä 
  */
 const Content = (props) =>  {
-  const paragraphs = props.partsAndExercises.map((e) => (<Part key={e.part} name={e.part} exercises={e.exercises}/>)); 
+  const paragraphs = props.partsAndExercises.map((e) => (<Part key={e.name} name={e.name} exercises={e.exercises}/>)); 
   return (<>{paragraphs}</>);
 
 
@@ -39,23 +39,30 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const partsAndExercises = [
-    {part: part1, exercises: exercises1}, 
-    {part: part2, exercises: exercises2}, 
-    {part: part3, exercises: exercises3}];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
-      <Header course={course}/>
-      <Content partsAndExercises={partsAndExercises}/>
-      <Total partsAndExercises={partsAndExercises}/>
+      <Header course={course.name}/>
+      <Content partsAndExercises={course.parts}/>
+      <Total partsAndExercises={course.parts}/>
     </div>
   )
 }
